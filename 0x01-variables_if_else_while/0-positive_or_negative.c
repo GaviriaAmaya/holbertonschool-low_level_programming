5b37330d372ce444a31adf73ac16cb97c7d910d3
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-08 21:27:10 +0000

    Adding else if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
  *
  * Return: Always success (0)
  */
+
 int main(void)
 
 {
@@ -17,15 +18,16 @@ int main(void)
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
-	if (n > 0)
+	else if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
