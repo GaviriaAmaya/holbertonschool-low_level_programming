2a6af7d5f00b30aead360826e3ea15bb11c07028
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-09 04:29:00 +0000

    Understanding te use of putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints numbers in base 10 without printf and char
+ *
+ * Return: Always success (0)
+ */
+
+int main(void)
+
+{
+int c;
+for (c = 48; c <= 57; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
