aee84ac41ee9d6d11734d3339df8ebb4b4af4dee
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-12 22:35:21 +0000

    Try with else if on 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,16 @@
 #include <ctype.h>
-# include "holberton.h"
+#include "holberton.h"
 
 int _islower(int c)
 {
-
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else if (c >= 97 && c <= 122)
+{
+return (0);
+}
+_putchar('\n');
+return (0);
 }
