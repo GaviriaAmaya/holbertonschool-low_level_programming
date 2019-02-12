702fcd7bfc256eddc453e75e539cb8fcb5356439
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-12 22:44:44 +0000

    Put only else and return

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,3 @@
-#include <ctype.h>
 #include "holberton.h"
 
 int _islower(int c)
@@ -7,10 +6,9 @@ if (c >= 65 && c <= 90)
 {
 return (1);
 }
-else if (c >= 97 && c <= 122)
+else
 {
 return (0);
 }
 _putchar('\n');
-return (0);
 }
