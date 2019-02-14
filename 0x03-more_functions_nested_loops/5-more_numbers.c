797039a7f085e3b1f770e438124fc9f77b64c385
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-14 21:51:00 +0000

    Get out

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -13,20 +13,18 @@ int b;
 int units;
 int dec;
 
-b = 0;
-while (b < 9)
+for (b = 0; b < 9; b++)
 {
-
 	for (a = 0; a <= 14; a++)
 		{
 			units = (a % 10);
 			dec = (a / 10);
 			_putchar (units + 48);
-			if (a >= 10)
+			if (a > 8)
 			{
 			_putchar (dec + 48);
 			}
 		}
-
 		_putchar ('\n');
+}
 }
