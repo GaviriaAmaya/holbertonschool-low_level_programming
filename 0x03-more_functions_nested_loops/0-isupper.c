72ecca7334fe3d8b95432737deff544fac07e7c9
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-14 17:44:02 +0000

    Renewed and corrected 11

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 /**
+ *_isupper - Prints 1 if the character is uppercase
+ *@c: Character
  *
- *
- *
- *
+ *Return: 1 if success
  */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c >= 90)
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return(1);
+		return (1);
 	}
 	_putchar ('\n');
 }
