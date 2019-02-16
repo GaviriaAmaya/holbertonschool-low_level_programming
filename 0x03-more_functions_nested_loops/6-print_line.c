d029c43318371e3cc09e4053f1f5331ab9ea973d
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-16 01:45:46 +0000

    Work at home

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,19 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * print_line - Prints a _ each n times
+ * @n: Determines How much _ prints
+ *Return: Void
  */
 
+void print_line(int n)
+{
+int line;
+
+while (line < n)
+{
+pline = line  n;
+_putchar(95);
+_putchar('\n');
+line++;
+}
+}
