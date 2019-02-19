d2b5dfec5e515fdc72eddfd86a2e3579218171ff
GaviriaAmaya <judamayaga@unal.edu.co>
2019-02-19 21:54:05 +0000

    Applying Betty

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
  *swap_int - Change the values from a to b, and visceversa
- *
- *
+ *@a: Pointer to an integer
+ *@b: Pointer to the integer to swap
  *
  */
 void swap_int(int *a, int *b)
