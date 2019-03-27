acb3e0e475a7ef53d3931b5d9371da1951bf62f5
GaviriaAmaya <judamayaga@unal.edu.co>
2019-03-27 01:06:16 +0000

    [Feature] Dear, This is your simple answer

---
+++ Pandora/reitero.c
@@ -0,0 +1,11 @@
+#include "reitero.h"
+/**
+ *main - Prints, through recursion, a reiteration
+ *
+ *Return: Always success. As my satisfaction to be yours
+ */
+int main(void)
+{
+	recursive_je_t_aime("Te reitero que te amo montones");
+	return (0);
+}
---
+++ Pandora/reitero.h
@@ -0,0 +1,4 @@
+#ifndef REITERO_H
+#define REITERO_H
+void recursive_je_t_aime(char *liebe);
+#endif
