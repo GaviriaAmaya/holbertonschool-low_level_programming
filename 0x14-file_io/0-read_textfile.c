d3f50d4d211df535453381154d6d793154fc8210
GaviriaAmaya <judamayaga@unal.edu.co>
2019-04-03 01:16:36 +0000

    [Feature] Add program that reads a file

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -22,6 +22,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc((sizeof(char) * letters));
 
+	if (buffer == NULL)
+		return (0);
+
 	size = read(fd, buffer, letters);
 
 	if (size < 0)
