e2967b941d86f6ba67b86f5ef9de5314d6bf074a
GaviriaAmaya <judamayaga@unal.edu.co>
2019-04-03 00:55:26 +0000

    [Feature] Add program that reads a file

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,15 +1,36 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "holberton.h"
 /**
  *read_textfile - Reads a file
  *@filename: Points to the file that wants to read
- *@letters: Save the amount of letters
- *Return: 
+ *@letters: Amount of chars: Buffer size
+ *Return: Amount of characters
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fd, size;
+	char *buffer;
 
-	fd = open(filename, 
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc((sizeof(char) * letters));
+
+	size = read(fd, buffer, letters);
+
+	if (size == -1)
+	{
+		close (size);
+		return (0);
+	}
+
+	write (1, buffer, letters);
+		close(fd);
+		return (size);
 }
