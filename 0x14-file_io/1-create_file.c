ba2b708eb715e2f4619fd45b0bd408be82671349
GaviriaAmaya <judamayaga@unal.edu.co>
2019-04-03 02:41:33 +0000

    [Feature] Add function that creates a file, and puts content in it

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 	{
 		close(fd);
-		return(-1);
+		return (-1);
 	}
 
 	if (text_content == NULL)
 	{
 		close(fd);
-		return(1);
+		return (1);
 	}
 	while (text_content[i] != '\0')
 		i++;
@@ -33,5 +33,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
-	return(1);
+	return (1);
 }
