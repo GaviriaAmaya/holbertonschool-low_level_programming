c8183112301e2b8be3b16800618929b836b7326c
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-07 05:14:38 +0000

    Another 100 lose

---
+++ 0x17-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isdigit - Print 1 if is digit
+ *@c: Character
+ *
+ *Return: Success if 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
