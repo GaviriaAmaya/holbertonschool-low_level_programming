c0097bd5d5f94c06c2e22a19b3d1c2f02efe6642
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-31 21:51:38 +0000

    [Feature] Add function that creates a Hash Table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,10 +8,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_h_t;
 
-	new_h_t = malloc(/*sizeof(unsigned long int) **/size);
+	new_h_t = malloc(sizeof(hash_table_t));
 
 	if (new_h_t == NULL)
 		return (NULL);
 
+	new_h_t->array = malloc(size);
+
+	if (new_h_t->array == NULL)
+	{
+		free (new_h_t);
+		return (NULL);
+	}
 	return (new_h_t);
 }
