d5fc733a6a6e3118137d830f50811cf0a194301a
GaviriaAmaya <judamayaga@unal.edu.co>
2019-06-18 02:45:39 +0000

    [Feature] Add selection algorithm

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -7,26 +7,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, aux, flag;
+	unsigned int i, j, index, aux;
 
 	if (array != '\0')
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < size - 1; i++)
 		{
-			flag = 0;
-			for (j = 0; j < size -1; j++)
+			index = i;
+			for (j = i + 1; j < size; j++)
 			{
-				if (array[j] > array[i])
+				if (array[j] < array[index])
 				{
-					aux = array[i];
-					array[i] = array[j];
-					array[j] = aux;
-					flag = 1;
-					print_array(array, size);
+					index = j;
 				}
+				aux = array[i];
+				array[i] = array[index];
+				array[index] = aux;
 			}
-			if (flag == 0)
-				break;
+			print_array(array, size);
 		}
 	}
 }
