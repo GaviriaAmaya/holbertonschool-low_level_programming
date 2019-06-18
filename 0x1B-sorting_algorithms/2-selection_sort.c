6ac294bb4cb1a00c9fd81d42831efabe4b7c251d
GaviriaAmaya <judamayaga@unal.edu.co>
2019-06-18 03:07:46 +0000

    [Fix] Put the swap out the quick loop

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -7,24 +7,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, index, aux;
+	unsigned int i, j, index, aux, flag;
 
 	if (array != '\0')
 	{
 		for (i = 0; i < size - 1; i++)
 		{
 			index = i;
+			flag = 0;
 			for (j = i + 1; j < size; j++)
 			{
 				if (array[j] < array[index])
 				{
 					index = j;
+					flag = 1;
 				}
 			}
 			aux = array[i];
 			array[i] = array[index];
 			array[index] = aux;
 			print_array(array, size);
+			if (flag == 0)
+				return;
 		}
 	}
 }
