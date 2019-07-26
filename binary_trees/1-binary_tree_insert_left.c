e237aa64785fcbc6fea04d0eacc15c9435c64dc4
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-26 14:31:38 +0000

    [To 14]

--- binary_trees/1-binary_tree_insert_left.c
+++ binary_trees/1-binary_tree_insert_left.c
@@ -1,47 +1,28 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_insert_left - Insert a left child node to a node.
- *
  * @parent: Address of the node to insert a left child.
  * @value: Value of the new node.
- *
  * Return: Address of the new node or NULL.
  */
-
-#include "binary_trees.h"
-
-/**
- * binary_tree_insert_left - Insert a left child node to a node.
- *
- * @parent: Address of the node to insert a left child.
- * @value: Value of the new node.
- *
- * Return: Address of the new node or NULL.
- */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new_node, *temp;
+	binary_tree_t *new_node, *aux;
 
-  if (parent == NULL)
-    return (NULL);
+	new_node = binary_tree_node(parent, value);
 
-  new_node = binary_tree_node(parent, value);
-  if (new_node == NULL)
-    return (NULL);
+	if (!new_node)
+		return (NULL);
 
-  if (parent->left != NULL)
-    {
-      temp = parent->left;
-      parent->left = new_node;
-      new_node->left = temp;
-      temp->parent = new_node;
-    }
-  else
-    {
-      parent->left = new_node;
-    }
+	if (parent->left)
+	{
+		aux = parent->left;
+		parent->left = new_node;
+		new_node->left = aux;
+		aux->parent = new_node;
+	}
+	else
+		parent->left = new_node;
 
-  return (new_node);
+	return (new_node);
 }
