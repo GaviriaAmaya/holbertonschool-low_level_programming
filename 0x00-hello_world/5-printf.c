0df019401fc2bf13d1540aff2d3f6e786635b6c3
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-27 23:08:24 +0000

    [Feature] Use of printf function and -Wall GCC flag

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Practice of printf. Also, -Wall on gcc
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	char *Dora = "with proper grammar, but the outcome is a piece of art,";
+
+		printf("%s\n", Dora);
+
+	return (0);
+}
