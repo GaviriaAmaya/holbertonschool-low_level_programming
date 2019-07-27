7759d866cac25d92f15af1b79cd4d5de6e531e1e
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-27 21:48:00 +0000

    Home

---
+++ binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - Measures thee balance of a binary tree
+ * @tree: Pointer to the root node
+ * Return: Number of branches derivated. If fails 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_b, right_b;
+
+
+}
