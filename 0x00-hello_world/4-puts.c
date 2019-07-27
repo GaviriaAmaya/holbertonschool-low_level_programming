3a9c23bc6f24401bb94c1cfb9dd83bf40ca7003f
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-27 22:46:31 +0000

    [Feature] Print a simple string with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Practice of the puts function
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
