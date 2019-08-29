090e479d5f9ff109508c715c103682c7c9001fc4
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-29 09:12:39 +0000

    [Feature] Function that sets the value of any integer to 98

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * reset_to_98 - Takes a number and resets to 98
+ * reset_to_98 - Takes an integer and resets to 98
  * @n: Pointer to an integer value
  * Return: Void
  */
