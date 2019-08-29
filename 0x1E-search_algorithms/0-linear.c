89769d42caf8c382ffe7ea061a1e89d48a48292a
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-29 21:18:05 +0000

    [Fix] Add checked value with printf.

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,17 +13,17 @@ int linear_search(int *array, size_t size, int value)
 
 	if (*array && array != NULL)
 	{
-		while(i < size)
+		while (i < size)
 		{
-			if (array[i] == value)
-				return(i);
-			else
+			if (array[i] != value)
 			{
 				printf("Value checked array[%d] = [%d]\n",
 				       really, array[i]);
 				really++;
 				i++;
 			}
+			else
+				return (i);
 		}
 	}
 
