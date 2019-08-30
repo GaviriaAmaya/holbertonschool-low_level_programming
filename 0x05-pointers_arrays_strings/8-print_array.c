fae96e3e1ec6d212da03e9778eff6d7b12c0c547
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-30 13:24:03 +0000

    [Fix] Correct if n is less than 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,7 +9,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[i]);
+	if (n >= 0)
+	{
+		for (i = 0; i < (n - 1); i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[i]);
+	}
+
+	else
+		return;
 }
