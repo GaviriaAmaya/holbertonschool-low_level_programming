44182bc0b1a308ee232e80dae5bd14e43a4ad988
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-30 13:17:41 +0000

    [Feature] Prints an array to the index

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_array - Print an array
+ * @a: Pointer to the first array position
+ * @n: Amount of elements t be printed
+ * Return: None
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+	printf("%d\n", a[i]);
+}
