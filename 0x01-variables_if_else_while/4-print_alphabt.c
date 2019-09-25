913c42f1a0f18bc3c280b4475a9791ac7d1bebd6
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-25 19:28:58 +0000

    [Feature] Print alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet without 'e' and 'q'
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 0; alphabet < 26; alphabet++)
+        {
+		if (alphabet == 4 || alphabet == 16)
+			alphabet++;
+		else
+		    putchar(alphabet + 97);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
