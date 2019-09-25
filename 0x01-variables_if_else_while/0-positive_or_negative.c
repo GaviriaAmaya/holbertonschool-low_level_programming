2d7b59f200ac26d1f16ed388a6939772a4c0d37c
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-25 18:42:42 +0000

    [Fix] Betty Style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - Function that proves if a random number is positive or not 
+/**
+ * main - Function that proves if a random number is positive or not
  * Return: Always success (0)
  */
 int main(void)
 {
-  int n;
+	int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-  if (n > 0)
-    printf("%d is positive\n", n);
-  else if (n < 0)
-    printf("%d is negative\n", n);
-  else
-    printf("%d is zero\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 
-  return (0);
+return (0);
 }
