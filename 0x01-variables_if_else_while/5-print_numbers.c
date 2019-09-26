b5bd370e26226c9265405aa4c1677f93640abb6c
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-26 02:13:21 +0000

    [Feature] Add two functions that print the numbers of one digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print the numbers from 0 to 9
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number + 48);
+
+	putchar('\n');
+
+	return (0);
+}
