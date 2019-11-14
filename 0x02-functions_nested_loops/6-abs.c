44b17407f2ade4d75b52584315a65ac594665ee4
GaviriaAmaya <judamayaga@unal.edu.co>
2019-11-14 01:44:04 -0500

    [Feature] Clone of abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _abs - Determines the absolute value of an integer
+ * @n: Integer to prove
+ * Return: Always success (0)
+ */
+int _abs(int n)
+{
+	return (n < 0 ? n * -1 : n);
+}
