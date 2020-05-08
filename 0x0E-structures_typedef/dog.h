a1e9bcf17db073bdf578ee2aba46ec8dad49978d
GaviriaAmaya <729@holbertonschool.com>
2020-05-08 17:28:57 -0500

    [Update] Redefined data type struct dog
    Typedef used to rename the dog. Now is called dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * @owner: String. Owner's name
  * Description: A puppy have to be nominated
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
