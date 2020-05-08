88205ec13b78b6787d38936c264fdbbc47cde79f
GaviriaAmaya <729@holbertonschool.com>
2020-05-08 02:02:53 -0500

    [Feature] Function that initialize the structure

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - Initialize a dog struct variable
+ * @d: Pointer to the dog structure
+ * @name: String: Fills the name field of the structure
+ * @age: Float: Age of the dog initialized
+ * @owner: String: Dog's Owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
