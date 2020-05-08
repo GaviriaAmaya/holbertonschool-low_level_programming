95b31dccb85ebd44d1826fb3172893cbb3cb43ea
GaviriaAmaya <729@holbertonschool.com>
2020-05-08 17:48:37 -0500

    [Feature] Create new_dog function
    Function that creates a new instance for the dog structure

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+/**
+ * new_dog - Function that creates a new "instance" from the dog struct
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: If success, a new dog. NULL if fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *the_name = name;
+	char *the_owner = owner;
+
+	dog_t *new = malloc(sizeof(dog_t));
+
+	if (!new)
+		return (NULL);
+
+	new->name = the_name;
+	new->age = age;
+	new->owner = the_owner;
+
+	return (new);
+}
